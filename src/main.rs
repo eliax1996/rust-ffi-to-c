2918d87e041b2107cbfe8be4ce5c526fec2c079b
Elia Migliore <elia.migliore@aiven.io>
2024-02-12 17:22:05 +0100

    example of stupid stuff done in unsafe rust :)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,39 @@
 extern crate core;
-use core::ffi::c_int;
+use core::ffi::c_char;
+use core::ffi::c_void;
 
 extern "C" {
-    fn multiply(a: c_int, b: c_int) -> c_int;
+    fn ops(a: *const c_char) -> c_void;
 }
 
-fn main() {
-    println!("[Rust] Hello from Rust! 🦀");
+struct Obj([u8; 8]);
 
+fn sub_fn() {
+
+    println!(
+        "Obj occupies {} bytes on the stack.",
+        std::mem::size_of::<Obj>()
+    );
+
+    let s = Obj([1,2,3,4,5,6,7,8]);
+    println!("s pointer: {:p}", &s);
     unsafe {
-        println!("[Rust] Calling function in C..");
+       let r = &s as *const _ as *const c_char;
+       ops(r);
+    };
+    std::mem::drop(s);
+    let r = Obj([11,12,13,14,15,16,17,18]);
+    println!("r pointer: {:p}", &r);
 
-        let result = multiply(5000, 5);
+    std::mem::drop(r);
+}
 
-        println!("[Rust] Result: {}", result);
-    }
+fn main()
+{
+  sub_fn();
+  sub_fn();
+  sub_fn();
+  sub_fn();
+  sub_fn();
+  sub_fn();
 }
--- src/multiply.c
+++ src/multiply.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int32_t multiply(int32_t a, int32_t b) {
-    printf("[C] Hello from C!\n");
-    printf("[C] Input a is: %i \n", a);
-    printf("[C] Input b is: %i \n", b);
-    printf("[C] Multiplying and returning result to Rust..\n");
+char* saved_ptr = NULL;
 
-    return a * b;
+void ops(char* ptr) {
+   if (saved_ptr != NULL) {
+        printf("[C] This was: %p \n", ((void *) saved_ptr));
+   } else {
+    printf("[C] This is: %p \n", ((void *) ptr));
+   }
+
+
+    saved_ptr = ptr;
+
+    return;
 }
